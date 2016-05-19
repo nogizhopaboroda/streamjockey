a3d520645b6890ba5555b7d079b1f938d1327b9d
Alexander Zhukov <a.zhukov@holidaypirates.com>
2016-05-19 17:15:08 +0200

    use config

--- index.js
+++ index.js
@@ -1,41 +1,74 @@
 var applescript = require('applescript');
 
-var func = function(){
-  var test='wow!';
-  document.querySelector('.player-control').click();
-  return test;
+var rules = {
+  //"mixcloud.com": {
+    //play: function(){
+      //document.querySelector('.player-control').click();
+      //return 'play';
+    //},
+    //pause: function(){
+      //document.querySelector('.player-control').click();
+      //return 'pause';
+    //},
+    //next: function(){},
+    //prev: function(){},
+    //is_playing: function(){},
+    //current_media_title: function(){}
+  //},
+  "vk.com/audios": {
+    play: function(){
+      document.querySelector('#ac_play').click();
+      return 'play';
+    },
+    pause: function(){
+      document.querySelector('#ac_play').click();
+      return 'pause';
+    },
+    next: function(){},
+    prev: function(){},
+    is_playing: function(){},
+    current_media_title: function(){}
+  }
 };
 
-var script = [
+var build_script = function(site, func){
+  return [
   'tell application "Google Chrome"',
     'set window_list to every window',            // get the windows
     'repeat with the_window in window_list',      // for every window
        'set tab_list to every tab in the_window', // get the tabs
        'repeat with the_tab in tab_list',         // for every tab
-          'if url of the_tab contains "mixcloud.com" then',
-              'log "bar"',
+          'if url of the_tab contains "' + site + '" then',
               'tell the_tab',
-                'set myVar to execute javascript "(' + func.toString() + ')()"',
-                'return myVar',
+                'set output to execute javascript "(' + func.toString() + ')()"',
+                'return output',
               'end tell',
           'end if',
        'end repeat',
     'end repeat',
   'end tell'
-].join('\n');
+  ].join('\n');
+}
 
-applescript.execString(script, function(err, rtn) {
-  if (err) {
-    // Something went wrong!
-    console.log(err);
+
+var play_pause = function(){
+  for(var site in rules){
+    var func = rules[site].play;
+    applescript.execString(build_script(site, func), function(err, rtn) {
+      if (err) {
+        // Something went wrong!
+        console.log(err);
+      }
+      console.log(rtn);
+    });
   }
-  console.log(rtn, err);
-});
+}
 
 
 var mediakeys = require('mediakeys').listen();
 mediakeys.on('play', function () {
-    console.log('play');
+    console.log('play/pause');
+    play_pause();
 })
 mediakeys.on('next', function () {
     console.log('next');
